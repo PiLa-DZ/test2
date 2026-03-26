fba380e7634522e9b49517418d4905c940ffc409
Nabil <Nabil@gmail.com>
2026-03-26 12:19:52 +0100

    Day 6

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("this test from day 3");
 console.log("this test from day 3");
 console.log("this test from day 4");
 console.log("this test from day 5");
+console.log("this test from day 6");
