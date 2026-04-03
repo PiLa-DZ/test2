221b54a7d9228a08e3ece6f2998446426a616ee8
Nabil <Nabil@gmail.com>
2026-04-03 10:25:57 +0100

    Revert "nabil-tester day 4"
    This reverts commit 2f20cff8b785b0180af0d0d916bc69cd787c4a4c.

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("this test from day 1");
 console.log("this test from day 2");
 console.log("this test from day 3"); // PiLa-DZ
 console.log("this test from day 4"); // Nabil-Tester
-console.log("this test from day 5"); // PiLa-DZ
