a9e82f7d967b8b0befa0b4c583efa51f3f6c2729
Nabil <Nabil@gmail.com>
2026-04-03 09:06:55 +0100

    pila-dz Day 5

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("this test from day 1");
 console.log("this test from day 2");
 console.log("this test from day 3"); // PiLa-DZ
 console.log("this test from day 4"); // Nabil-Tester
+console.log("this test from day 5"); // PiLa-DZ
